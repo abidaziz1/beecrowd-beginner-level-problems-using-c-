947c60b0284e6e2b07ca18308ea156b9248910ae
Abid Aziz <139110271+abidaziz1@users.noreply.github.com>
2024-08-20 02:39:22 +0600

    Create Difference.cpp

---
+++ Difference.cpp
@@ -0,0 +1,16 @@
+"""
+  Problem No: 7
+Name: Difference
+ID: 1007
+  """
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+int main() {
+    int A,B,C,D;
+    cin>>A>>B>>C>>D;
+    int diff= (A*B-C*D);
+    cout<<"DIFERENCA = "<<diff<<endl;
+    return 0;
+}
