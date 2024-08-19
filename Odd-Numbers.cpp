a27f78e833109eb4c9dc06cf8960af437d9c605c
Abid Aziz <139110271+abidaziz1@users.noreply.github.com>
2024-08-20 02:54:16 +0600

    Create Odd-Numbers.cpp

---
+++ Odd-Numbers.cpp
@@ -0,0 +1,14 @@
+"""
+  Problem No: 38
+Name: Odd Numbers
+ID: 1067
+  """
+#include<iostream>
+using namespace std;
+int main() {
+    for(int i = 1; i <= 100; i++) {
+        if(i % 2 != 0)
+            cout << i << endl;
+    }
+    return 0;
+}
