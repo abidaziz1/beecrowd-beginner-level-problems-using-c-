5552e14f4904eb3672b61b1b46fcbd0be2d976a5
Abid Aziz <139110271+abidaziz1@users.noreply.github.com>
2024-08-20 02:34:31 +0600

    Create Extremely-Basic.cpp

---
+++ Extremely-Basic.cpp
@@ -0,0 +1,12 @@
+"""Problem No: 2
+Name: Extremely Basic
+ID: 1001"""
+#include<iostream>
+using namespace std;
+int main() {
+    int X, n1, n2;
+    cin>>n1>>n2;
+    X= n1+n2;
+    cout<<"X = "<<X<<endl;
+    return 0;
+}
