257049fc7a7e30dccd285d947082cbe03a90cfb8
Abid Aziz <139110271+abidaziz1@users.noreply.github.com>
2024-08-20 03:06:52 +0600

    Create Sequence 2.cpp

---
+++ Sequence 2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    for(int i = 1, j = 7; i <= 9; i += 2, j += 2) {
+        cout << "I=" << i << " J=" << j << endl;
+    }
+    return 0;
+}
