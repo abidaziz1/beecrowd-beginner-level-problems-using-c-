f2b89bcf3fb21e37bf37c9a9dc7f644b7fb6987e
Abid Aziz <139110271+abidaziz1@users.noreply.github.com>
2024-08-20 02:35:16 +0600

    Create Simple-Sum.cpp

---
+++ Simple-Sum.cpp
@@ -0,0 +1,14 @@
+"""
+  Problem No: 3
+Name: Simple Sum
+ID: 1003
+  """
+  #include<iostream>
+using namespace std;
+int main() {
+    int A,B,r;
+    cin>>A>>B;
+    r= A+B;
+    cout<<"SOMA = "<<r<<endl;
+    return 0;
+}
