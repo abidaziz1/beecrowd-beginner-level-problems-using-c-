e0b366d5a351d7fe782f610312cf678e9491e2d1
Abid Aziz <139110271+abidaziz1@users.noreply.github.com>
2024-08-20 02:36:01 +0600

    Create Simple-Product.cpp

---
+++ Simple-Product.cpp
@@ -0,0 +1,12 @@
+"""Problem No: 4
+Name: Simple Product
+ID: 1004"""
+#include<iostream>
+using namespace std;
+int main() {
+    int a,b,r;
+    cin>>a>>b;
+    r=a*b;
+    cout<<"PROD = "<<r<<endl;
+    return 0;
+}
