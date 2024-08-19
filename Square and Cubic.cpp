1c0ab04542e9633b47d67ea24a689da1d4eb12bd
Abid Aziz <139110271+abidaziz1@users.noreply.github.com>
2024-08-20 03:10:39 +0600

    Create Square and Cubic.cpp

---
+++ Square and Cubic.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    int N;
+    scanf("%d", &N);
+    for(int i=1; i<=N; i++) {
+        printf("%d %d %d\n", i, i*i, i*i*i &#8203;:contentReference[oaicite:0]{index=0}&#8203;
