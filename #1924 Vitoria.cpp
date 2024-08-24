e964dd16927b8923d5eb71e802e7e59ad165eb46
Abid Aziz <139110271+abidaziz1@users.noreply.github.com>
2024-08-24 20:42:42 +0600

    Create #1924 Vitoria.cpp

---
+++ #1924 Vitoria.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int n; cin>>n; string name;
+    for(int i=0; i<n; i++){
+        cin>>name;
+    }
+    cout<<"Ciencia da Computacao"<<endl;
+
+    return 0;
+}
