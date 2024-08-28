2e75bb1d433b87088bf1459fbe518673afac0306
Abid Aziz <139110271+abidaziz1@users.noreply.github.com>
2024-08-28 21:10:47 +0600

    Create #1589 Bob Conduit.cpp

---
+++ #1589 Bob Conduit.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int R1, R2;
+        cin >> R1 >> R2;
+        cout << R1 + R2 << endl;
+    }
+    return 0;
+}
