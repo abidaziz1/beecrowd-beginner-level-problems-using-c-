c98256b069ad7c12a965da126fc48d2cc87f1baa
Abid Aziz <139110271+abidaziz1@users.noreply.github.com>
2024-08-29 19:12:02 +0600

    Create #2581 I am Toorg.py

---
+++ #2581 I am Toorg.py
@@ -0,0 +1,16 @@
+'''
+Toorg is the wisest member of the group of heroes called The Protectors of the Milky Way. For any question, it has the ideal answer! Write a program that, given a question, tells Toorg's answer.
+
+Input
+The input will have several test cases. For each test case, a N number is displayed, which represents the number of test cases. Then there will be N lines, with questions asked for Toorg.
+
+Output
+For each test case, print Toorg's answer.
+'''
+# Read the number of questions
+n = int(input())
+
+# Loop over each question and output "I am Toorg!" for each one
+for _ in range(n):
+    input()  # Read the question (but do nothing with it)
+    print("I am Toorg!")
